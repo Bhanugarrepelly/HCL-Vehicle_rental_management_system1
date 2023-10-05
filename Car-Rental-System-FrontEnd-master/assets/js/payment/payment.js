bcf2727ad791e3df18c4e40ac77fdf8f1c2f141f
Bhanugarrepelly <garrepellibhanu123@gmail.com>
2023-10-06 00:15:19 +0530

    firstcommit

--- Car-Rental-System-FrontEnd-master/assets/js/payment/payment.js
+++ Car-Rental-System-FrontEnd-master/assets/js/payment/payment.js
@@ -4,6 +4,7 @@ const paragraphEle=document.getElementById("paraCal");
 function calculate(){
     let valueInput=parseInt(inputValue.value);
     let caluculatedValue=valueInput*30;
-    paragraphEle.textContent="You have to pay the amount of ₹"+caluculatedValue;
+    paragraphEle.textContent="You have to pay the amount of "+caluculatedValue+"/-";
     paragraphEle.style.color="red";
+    paragraphEle.style.fontSize="20px";
 }
